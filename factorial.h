6c1e80e677035379d674ac84b5ce31106e8d8f6e
Prasanna Timsina <69035229+macha57@users.noreply.github.com>
2024-10-16 13:57:50 +0545

    Update factorial.h

--- factorial.h
+++ factorial.h
@@ -2,6 +2,7 @@
 Author: <pratimsina@gmail.com>
 Version: [1.0]
 
+Notice: Since the program has inefficient memory utiliztion,this version does not support very large factorials [It must not exceed 4 bytes]
 
 This program prints the factorial of a number which is true for all natural numbers (Z∈N) where Z are the natural numbers form (0,∞) and N 
 represents the sets of natural numbers; i.e. N = {0,1,2,3,...,∞}. 
@@ -13,11 +14,28 @@ represents the sets of natural numbers; i.e. N = {0,1,2,3,...,∞}.
 
 int factorial(int a)
 {
-	int i,res=1;
-	for(i=1;i<=a;i++)
+	if(a<0)
 	{
-		res*=i;
+		printf("[INPUT-ERROR] ==> Cannot find the factorial of %d since it is a negative number\n",a);
+		return -1;
 	}
-	printf("%d is the factorial of %d\n",res,a );
-	return res;
+	else{
+		int i,res=1;
+		for(i=1;i<=a;i++)
+		{
+			res*=i;
+		}
+		if(a<=12)
+		{
+			printf("%d is the factorial of %d\n",res,a );
+			return res;
+		}
+		else
+		{
+			printf("[PROGRAM-ERROR] ==> The size of your integer exceeds 4 bytes ");
+			return -1;
+		}
+		
+	}
+
 }
